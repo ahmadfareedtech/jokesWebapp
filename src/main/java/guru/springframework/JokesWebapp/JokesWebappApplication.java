f258142923af1259b89e198256f1c092d5397f56
ahmadfareed <a.fareed@pioneerlogics.com>
2022-05-12 11:55:44 +0500

    initial commit

---
+++ src/main/java/guru/springframework/JokesWebapp/JokesWebappApplication.java
@@ -0,0 +1,13 @@
+package guru.springframework.JokesWebapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JokesWebappApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JokesWebappApplication.class, args);
+	}
+
+}
---
+++ src/test/java/guru/springframework/JokesWebapp/JokesWebappApplicationTests.java
@@ -0,0 +1,13 @@
+package guru.springframework.JokesWebapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JokesWebappApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
